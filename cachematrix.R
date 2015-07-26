f3f519f794619f056fad2d79f737a493e393c8bb
JorveU <akutanner@gmail.com>
2015-07-26 13:49:51 -0700

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##mackCachematrix
+##cache matrix saves the the matrix that is given into memory and invers
+##it this will allow the matric to be compared to new matrixes
+##ma will be the given matrix
+##this should be called once unless there is a new matrix
+makeCacheMatrix <- function(ma = matrix())
+{
+  ##x set to null but will cache the previous matrix
+  ##and will hold the inverse
+  x <- NULL
+  set <- function(y)
+  {
+    ma <<- y
+    x <<- NULL
+  }
+  get <- function() ma
+  setinverse <- function(solve) x <<- solve
+  getinverse <- function() x
+  ##this will be returned and used by cache solve
+  list(set = set, get = get, 
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve
+##uses the output from makeache to cache the matrix
+##returns the inverse of the orginal matrix
+cacheSolve <- function(ma, ...)
+{
+  x <- ma$getinverse()
+  ##check to see if the matrix has been calculated
+  if(!is.null(x))
+  {
+    ##send the chached matrix back
+    message("Retrieving cached data")
+    return(x)
+  }
+  ##else calculate the inverse
+  matrix.data <- ma$get()
+  x <- solve(matrix.data, ...)
+  
+  ma$setinverse(x)
+  
+  return(x)
 }
